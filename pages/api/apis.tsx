c7be8b85072b248d09c4ef90764ff05d48c39581
parth.khimani <parthkhimani48@gmail.com>
2023-09-13 12:30:30 +0530

    few changes

--- pages/api/apis.tsx
+++ pages/api/apis.tsx
@@ -5,7 +5,7 @@ const token = Cookies.get("token");
 
 export const getManagerData = async () => {
   const managerResponse = await fetch(
-    "http://localhost:3333/admin-dashboard/manager-data",
+    "https://quiz-app-backend-parthkhimani.vercel.app/admin-dashboard/manager-data",
     {
       method: "POST",
       headers: {
@@ -20,7 +20,7 @@ export const getManagerData = async () => {
 
 export const getEmployeeData = async () => {
   const employeeResponse = await fetch(
-    "http://localhost:3333/admin-dashboard/employee-data",
+    "https://quiz-app-backend-parthkhimani.vercel.app/admin-dashboard/employee-data",
     {
       method: "POST",
       headers: {
@@ -36,7 +36,7 @@ export const getEmployeeData = async () => {
 export const deleteManagerFn = async (managerJSONString: string) => {
   const data = managerJSONString;
   const response = await fetch(
-    "http://localhost:3333/admin-dashboard/delete-manager-data",
+    "https://quiz-app-backend-parthkhimani.vercel.app/admin-dashboard/delete-manager-data",
     {
       method: "POST",
       headers: {
@@ -54,7 +54,7 @@ export const deleteManagerFn = async (managerJSONString: string) => {
 export const deleteEmployeeFn = async (employeeJSONString: string) => {
   const data = employeeJSONString;
   const response = await fetch(
-    "http://localhost:3333/admin-dashboard/delete-employee-data",
+    "https://quiz-app-backend-parthkhimani.vercel.app/admin-dashboard/delete-employee-data",
     {
       method: "POST",
       headers: {
@@ -70,104 +70,128 @@ export const deleteEmployeeFn = async (employeeJSONString: string) => {
 };
 
 export const updateManagerFn = async (formData: FormData) => {
-  const response = await fetch("http://localhost:3333/update-manager", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/update-manager",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const UpdateEmployeeFn = async (formData: FormData) => {
-  const response = await fetch("http://localhost:3333/update-employee", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/update-employee",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const AddManagerFn = async (formData: FormData) => {
-  const response = await fetch("http://localhost:3333/add-manager", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/add-manager",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const AddEmployeeFn = async (formData: FormData) => {
-  const response = await fetch("http://localhost:3333/add-employee", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/add-employee",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const GetQuizFn = async (employee: string) => {
-  const response = await fetch("http://localhost:3333/get-quiz", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ employee: employee, token: token }),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/get-quiz",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ employee: employee, token: token }),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const GetQuizByTechnologyFn = async (technology: string) => {
-  const response = await fetch("http://localhost:3333/get-quiz-by-technology", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ technology: technology, token: token }),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/get-quiz-by-technology",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ technology: technology, token: token }),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const AssignQuizFn = async (quiz: string, employee: string) => {
-  const response = await fetch("http://localhost:3333/assign-auiz", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ quiz: quiz, employee: employee, token: token }),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/assign-auiz",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ quiz: quiz, employee: employee, token: token }),
+    }
+  );
   const result = response.json();
   return result;
 };
 
 export const AbandonQuizFn = async (quiz: string, employee: string) => {
-  const response = await fetch("http://localhost:3333/abandon-auiz", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ quiz: quiz, employee: employee, token: token }),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/abandon-auiz",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ quiz: quiz, employee: employee, token: token }),
+    }
+  );
   const result = response.json();
   return result;
 };
 
 export const GetEmployeeFn = async (employee: string) => {
   const response = await fetch(
-    "http://localhost:3333/get-employee-data-to-assign-quiz",
+    "https://quiz-app-backend-parthkhimani.vercel.app/get-employee-data-to-assign-quiz",
     {
       method: "POST",
       headers: {
@@ -181,53 +205,65 @@ export const GetEmployeeFn = async (employee: string) => {
 };
 
 export const AdminLoginFn = async (values: MyLoginValue) => {
-  const AdminResponse = await fetch("http://localhost:3333/admin-login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const AdminResponse = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/admin-login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const AdminLoginData = await AdminResponse.json();
   return AdminLoginData;
 };
 
 export const ManagerLoginFn = async (values: MyLoginValue) => {
-  const ManagerResponse = await fetch("http://localhost:3333/manager-login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const ManagerResponse = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/manager-login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const ManagerLoginData = await ManagerResponse.json();
   return ManagerLoginData;
 };
 
 export const EmployeeLoginFn = async (values: MyLoginValue) => {
-  const EmployeeResponse = await fetch("http://localhost:3333/employee-login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const EmployeeResponse = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/employee-login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const EmployeeLoginData = await EmployeeResponse.json();
   return EmployeeLoginData;
 };
 
 export const GetQuizDataFn = async (quizIndex: string, employee: string) => {
-  const quiz = await fetch("http://localhost:3333/get-quiz-data", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({
-      index: quizIndex,
-      employee: employee,
-      token: token,
-    }),
-  });
+  const quiz = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/get-quiz-data",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        index: quizIndex,
+        employee: employee,
+        token: token,
+      }),
+    }
+  );
   const quizData = await quiz.json();
   return quizData;
 };
@@ -236,29 +272,35 @@ export const GetQuizDataWithAnswersFn = async (
   quizIndex: string,
   employee: string
 ) => {
-  const quiz = await fetch("http://localhost:3333/get-quiz-data-with-answers", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({
-      index: quizIndex,
-      employee: employee,
-      token: token,
-    }),
-  });
+  const quiz = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/get-quiz-data-with-answers",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        index: quizIndex,
+        employee: employee,
+        token: token,
+      }),
+    }
+  );
   const quizData = await quiz.json();
   return quizData;
 };
 
 export const SignUpFn = async (values: MySignUpValue) => {
-  const response = await fetch("http://localhost:3333/employee-register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const response = await fetch(
+    "https://quiz-app-backend-parthkhimani.vercel.app/employee-register",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const result = await response.json();
   return result;
 };
