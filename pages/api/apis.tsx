9f894b6fa986f5f07ddbd6ea2aa5760b7ed04ac9
parth.khimani <parthkhimani48@gmail.com>
2023-09-27 19:03:39 +0530

    few changes

--- pages/api/apis.tsx
+++ pages/api/apis.tsx
@@ -1,9 +1,14 @@
 import { MyLoginValue, MySignUpValue } from "@/types";
-import "dotenv/config";
+import Cookies from "js-cookie";
 
 export const getManagerData = async () => {
   const managerResponse = await fetch(
-    `${process.env.BASE_URL}admin-dashboard/manager-data`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/admin-dashboard/manager-data`,
+    {
+      headers: new Headers({
+        auth: Cookies.get("token") as string,
+      }),
+    }
   );
   const managerData = await managerResponse.json();
   return managerData;
@@ -11,7 +16,12 @@ export const getManagerData = async () => {
 
 export const getEmployeeData = async () => {
   const employeeResponse = await fetch(
-    `${process.env.BASE_URL}/admin-dashboard/employee-data`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/admin-dashboard/employee-data`,
+    {
+      headers: new Headers({
+        auth: Cookies.get("token") as string,
+      }),
+    }
   );
   const employeeData = await employeeResponse.json();
   return employeeData;
@@ -20,7 +30,7 @@ export const getEmployeeData = async () => {
 export const deleteManagerFn = async (managerJSONString: string) => {
   const data = managerJSONString;
   const response = await fetch(
-    `${process.env.BASE_URL}/admin-dashboard/delete-manager-data`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/admin-dashboard/delete-manager-data`,
     {
       method: "POST",
       headers: {
@@ -38,7 +48,7 @@ export const deleteManagerFn = async (managerJSONString: string) => {
 export const deleteEmployeeFn = async (employeeJSONString: string) => {
   const data = employeeJSONString;
   const response = await fetch(
-    `${process.env.BASE_URL}/admin-dashboard/delete-employee-data`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/admin-dashboard/delete-employee-data`,
     {
       method: "POST",
       headers: {
@@ -54,88 +64,119 @@ export const deleteEmployeeFn = async (employeeJSONString: string) => {
 };
 
 export const updateManagerFn = async (formData: FormData) => {
-  const response = await fetch(`${process.env.BASE_URL}/update-manager`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/update-manager`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const UpdateEmployeeFn = async (formData: FormData) => {
-  const response = await fetch(`${process.env.BASE_URL}/update-employee`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/update-employee`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const AddManagerFn = async (formData: FormData) => {
-  const response = await fetch(`${process.env.BASE_URL}/add-manager`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/add-manager`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const AddEmployeeFn = async (formData: FormData) => {
-  const response = await fetch(`${process.env.BASE_URL}/add-employee`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(Object.fromEntries(formData)),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/add-employee`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    }
+  );
   const result = await response.json();
   return result;
 };
 
 export const GetQuizFn = async (employee: string) => {
-  const response = await fetch(`${process.env.BASE_URL}/get-quiz`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/get-quiz`, {
+    headers: new Headers({
+      auth: Cookies.get("token") as string,
+    }),
+  });
   const result = await response.json();
   return result;
 };
 
 export const GetQuizByTechnologyFn = async (technology: string) => {
   const response = await fetch(
-    `${process.env.BASE_URL}/get-quiz-by-technology`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/get-quiz-by-technology`,
+    {
+      headers: new Headers({
+        auth: Cookies.get("token") as string,
+      }),
+    }
   );
   const result = await response.json();
   return result;
 };
 
 export const AssignQuizFn = async (quiz: string, employee: string) => {
-  const response = await fetch(`${process.env.BASE_URL}/assign-auiz`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/assign-auiz`,
+    {
+      headers: new Headers({
+        auth: Cookies.get("token") as string,
+      }),
+    }
+  );
   const result = response.json();
   return result;
 };
 
 export const AbandonQuizFn = async (quiz: string, employee: string) => {
-  const response = await fetch(`${process.env.BASE_URL}/abandon-auiz`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ quiz: quiz, employee: employee }),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/abandon-auiz`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ quiz: quiz, employee: employee }),
+    }
+  );
   const result = response.json();
   return result;
 };
 
 export const GetEmployeeFn = async (employee: string) => {
   const response = await fetch(
-    `${process.env.BASE_URL}/get-employee-data-to-assign-quiz`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/get-employee-data-to-assign-quiz`,
     {
       method: "POST",
       headers: {
@@ -149,32 +190,38 @@ export const GetEmployeeFn = async (employee: string) => {
 };
 
 export const AdminLoginFn = async (values: MyLoginValue) => {
-  const AdminResponse = await fetch(`${process.env.BASE_URL}/admin-login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const AdminResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/admin-login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const AdminLoginData = await AdminResponse.json();
   return AdminLoginData;
 };
 
 export const ManagerLoginFn = async (values: MyLoginValue) => {
-  const ManagerResponse = await fetch(`${process.env.BASE_URL}/manager-login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const ManagerResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/manager-login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const ManagerLoginData = await ManagerResponse.json();
   return ManagerLoginData;
 };
 
 export const EmployeeLoginFn = async (values: MyLoginValue) => {
   const EmployeeResponse = await fetch(
-    `${process.env.BASE_URL}/employee-login`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/employee-login`,
     {
       method: "POST",
       headers: {
@@ -188,16 +235,19 @@ export const EmployeeLoginFn = async (values: MyLoginValue) => {
 };
 
 export const GetQuizDataFn = async (quizIndex: string, employee: string) => {
-  const quiz = await fetch(`${process.env.BASE_URL}/get-quiz-data`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({
-      index: quizIndex,
-      employee: employee,
-    }),
-  });
+  const quiz = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/get-quiz-data`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        index: quizIndex,
+        employee: employee,
+      }),
+    }
+  );
   const quizData = await quiz.json();
   return quizData;
 };
@@ -207,7 +257,7 @@ export const GetQuizDataWithAnswersFn = async (
   employee: string
 ) => {
   const quiz = await fetch(
-    `${process.env.BASE_URL}/get-quiz-data-with-answers`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/get-quiz-data-with-answers`,
     {
       method: "POST",
       headers: {
@@ -224,13 +274,16 @@ export const GetQuizDataWithAnswersFn = async (
 };
 
 export const SignUpFn = async (values: MySignUpValue) => {
-  const response = await fetch(`${process.env.BASE_URL}/employee-register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(values),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/employee-register`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(values),
+    }
+  );
   const result = await response.json();
   return result;
 };
